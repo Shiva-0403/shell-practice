9065141fb992b2bcc98e4adab34f836c44212658
shiva <shiva@example.com>
2025-09-22 04:34:14 -0500

    Special.characters

--- 08.spcl.variable.sh
+++ 08.spcl.variable.sh
@@ -3,3 +3,5 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
